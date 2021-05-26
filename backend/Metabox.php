8b537e8520ee738c4f38526743e7320ad0aae573
Jhon Marreros Guzman <j_marreros@hotmail.com>
2021-05-26 16:53:00 -0500

    Agregado consistencia CPT para eventos

--- backend/Metabox.php
+++ backend/Metabox.php
@@ -25,13 +25,15 @@ class Metabox{
 	public function show_metabox( $post ){
 		$post_id = $post->ID;
 		$enable_convivientes = get_post_meta($post_id, DCMS_ENABLE_CONVIVIENTES, true );
-        include_once ('views/metabox.php');
+		include_once ('views/metabox.php');
 	}
 
 	// Save data metabox
 	public function save_metabox_data( $post_id ){
-		$enable_convivientes = isset($_POST[DCMS_ENABLE_CONVIVIENTES])?1:0;
-		update_post_meta($post_id, DCMS_ENABLE_CONVIVIENTES, $enable_convivientes );
+		if ( get_post_type($post_id) === DCMS_EVENT_CPT ){
+			$enable_convivientes = isset($_POST[DCMS_ENABLE_CONVIVIENTES])?1:0;
+			update_post_meta($post_id, DCMS_ENABLE_CONVIVIENTES, $enable_convivientes );
+		}
 	}
 
 
